79c0d9695033dfd9bd951bee9dab9d15344e914a
MAlexGG <alexandragalarzag@gmail.com>
2024-09-06 21:24:26 +0200

    exceptions

--- src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ltp.contacts.service;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.exception.ContactNotFoundException;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
@@ -13,7 +15,6 @@ public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
-    
 
     @Override
     public Contact getContactById(String id) {
@@ -25,12 +26,28 @@ public class ContactServiceImpl implements ContactService {
         contactRepository.saveContact(contact);
     }
 
+    @Override
+    public void updateContact(String id, Contact contact) {
+        contactRepository.updateContact(findIndexById(id), contact);
+    }
+
+    @Override
+    public void deleteContact(String id) {
+        contactRepository.deleteContact(findIndexById(id));
+    }
+
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
+    }
 
     private int findIndexById(String id) {
         return IntStream.range(0, contactRepository.getContacts().size())
             .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(() -> new ContactNotFoundException(id));
     }
+
+    
 
 }
--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -1,18 +1,22 @@
 package com.ltp.contacts.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -21,17 +25,34 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
+    @GetMapping("/contact/all")
+    public ResponseEntity<List<Contact>> getContacts() {
+        List<Contact> contacts = contactService.getContacts();
+        return new ResponseEntity<>(contacts, HttpStatus.OK);
+    }
+
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
-        return new ResponseEntity<>(contact, HttpStatus.OK);
+        return new ResponseEntity<>(contact, HttpStatus.OK);   
     }
     
     @PostMapping("/contact")
-    public ResponseEntity<HttpStatus> createContact(Contact contact) {
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
         contactService.saveContact(contact);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(contact, HttpStatus.CREATED);
     }
     
+    @PutMapping("contact/{id}")
+    public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
+        contactService.updateContact(id, contact);
+        return new ResponseEntity<>(contactService.getContactById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/contact/{id}")
+    public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id){
+        contactService.deleteContact(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    } 
 
 }
