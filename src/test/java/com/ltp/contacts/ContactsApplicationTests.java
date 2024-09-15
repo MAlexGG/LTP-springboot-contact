d8fd19505052435c43f4f099faa811438bc53bd0
MAlexGG <alexandragalarzag@gmail.com>
2024-09-15 16:56:09 +0200

    test getContactById

--- src/test/java/com/ltp/contacts/ContactsApplicationTests.java
+++ src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -1,13 +1,79 @@
 package com.ltp.contacts;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.ltp.contacts.pojo.Contact;
+import com.ltp.contacts.repository.ContactRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ContactsApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ContactRepository contactRepository;
+
+	private Contact[] contacts = new Contact[] {
+		new Contact("1", "Jon Snow", "6135342524"),
+		new Contact("2", "Tyrion Lannister", "4145433332"),
+		new Contact("3", "The Hound", "3452125631")
+	};
+
+	@BeforeEach
+	void setup(){
+		for (int i = 0; i < contacts.length; i++) {
+			contactRepository.saveContact(contacts[i]);
+		}
+	}
+
+	@AfterEach
+	void clear(){
+		contactRepository.getContacts().clear();
+	}
+
 	@Test
-	void contextLoads() {
+	public void getContactByIdTest() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/contact/1");
+		mockMvc.perform(request)
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.name").value(contacts[0].getName()))
+			.andExpect(jsonPath("$.phoneNumber").value(contacts[0].getPhoneNumber()));
+	}
+	
+	@Test
+	public void getAllContactsTest() throws Exception {
+
+	}
+
+	@Test
+	public void validContactCreation() throws Exception {
+
+	}
+
+	@Test
+	public void invalidContactCreation() throws Exception {
+
+	}
+
+	@Test
+	public void contactNotFoundTest() throws Exception {
+
 	}
 
 }
